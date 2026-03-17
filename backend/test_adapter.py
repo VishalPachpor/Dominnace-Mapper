e2a5bbfe119e1008c13b92b78c8c62a423350bff
VishalPachpor <Vishalpatil080502@gmail.com>
2026-03-17 15:51:31 +0530

    chore: cleanup scratch files

--- backend/test_adapter.py
+++
@@ -1,8 +0,0 @@
-import asyncio
-from app.services.metaapi_service import get_symbol_specification
-
-async def main():
-    spec = await get_symbol_specification('e496a759-9b7e-46bd-acd8-3add8fe030e2', 'BTCUSD')
-    print("Spec:", spec.get("digits"), spec.get("point"), spec.get("stopLevel"), spec.get("spread"))
-
-asyncio.run(main())
